29b46c7f30b7e89e8ece654f9ef23c757b6efef5
Diego Bengochea <bengo.bengo@protonmail.com>
2024-06-25 16:02:17 +0200

    Cross canopy height with biomass estimates

---
+++ compile_height_biomass_data.py
@@ -0,0 +1,40 @@
+import pandas as pd 
+import geopandas as gpd
+from geoutils import add_feature
+from pathlib import Path
+
+ifn4_biomass_dir = 'ifn4_biomass/'
+ifn4_groups_utm = Path(ifn4_biomass_dir).glob('*.shp')
+
+pnoa_file_info = pd.read_csv('PNOA2_flights/maps_year_table.csv')
+
+for fname in ifn4_groups_utm:
+
+    ifn4 = gpd.read_file(fname)
+    crs = str(ifn4.crs)
+    utm = crs[-2:]
+
+    pnoa_info_filtered = pnoa_file_info[pnoa_file_info['utm'] == int(utm)]
+
+    print(pnoa_info_filtered)
+    print(pnoa_info_filtered['years'].unique())
+    for year in pnoa_info_filtered['years'].unique():
+
+        pnoa_info_filtered_year = pnoa_info_filtered[pnoa_info_filtered['years']==year]
+        ifn4_year = ifn4[ifn4['year']==year]
+
+        if len(ifn4_year.index)==0:
+            continue
+
+        ifn4_year_canopy = ifn4_year.copy()
+        for index, row in pnoa_info_filtered_year.iterrows():
+            fstem = row['file']
+            path = 'PNOA2_LiDAR_Vegetation_25m/{}.tif'.format(fstem)
+
+            print(ifn4_year_canopy)
+            ifn4_year_canopy = add_feature(ifn4_year_canopy,'h_{}'.format(index),path)    
+            print(ifn4_year_canopy)
+            break
+        break
+    
+
