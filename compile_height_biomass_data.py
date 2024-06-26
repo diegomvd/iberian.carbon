3030a13d512858069af1f09d1d768611d04d3946
Diego Bengochea <bengo.bengo@protonmail.com>
2024-06-26 16:43:29 +0200

    Height retrieval done

--- compile_height_biomass_data.py
+++ compile_height_biomass_data.py
@@ -2,11 +2,20 @@ import pandas as pd
 import geopandas as gpd
 from geoutils import add_feature
 from pathlib import Path
+import numpy as np 
 
 ifn4_biomass_dir = 'ifn4_biomass/'
 ifn4_groups_utm = Path(ifn4_biomass_dir).glob('*.shp')
 
 pnoa_file_info = pd.read_csv('PNOA2_flights/maps_year_table.csv')
+
+def remove_nans(array):
+    if len(array)>1:
+        array_new = array[~np.isnan(array)]
+    else:
+        array_new = array.copy()
+
+    return array_new
 
 for fname in ifn4_groups_utm:
 
@@ -32,7 +41,13 @@ for fname in ifn4_groups_utm:
             ifn4_year_canopy = add_feature(ifn4_year_canopy,'h_{}'.format(index),path)    
         
         ifn4_year_canopy['height'] = ifn4_year_canopy.filter(like='h_').apply(lambda row: row.tolist(), axis=1)
-        print(ifn4_year_canopy['height'].unique())
+        ifn4_year_canopy['height'] = ifn4_year_canopy['height'].apply(lambda hlist: np.unique(hlist) )
+        ifn4_year_canopy['height'] = ifn4_year_canopy['height'].apply(lambda harray: np.where(harray<0,np.nan,harray))
+        ifn4_year_canopy['height'] = ifn4_year_canopy['height'].apply(lambda harray: remove_nans(harray)[0])
+
+        ifn4_year_canopy = ifn4_year_canopy[ifn4_year_canopy.columns.drop(list(ifn4_year_canopy.filter(regex='h_')))]
+        
+        print(len(ifn4_year_canopy['height'].unique()))
         
     
 
