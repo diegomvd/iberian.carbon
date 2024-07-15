b886a30932ced8f93773f573d61b58d4fb808d93
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-07-15 11:00:18 +0200

    Housekeeping

--- pnoa_maps_flight_year.py
+++
@@ -1,5 +0,0 @@
-import geopandas as gpd
-from pathlib import Path
-import re 
-
-
--- test_ifn_projection.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-
-print("start")
-ifn = gpd.read_file('ifn4_filtered.shp')
-print(ifn)
-ifn = ifn.to_crs("EPSG:4326")
-print(ifn)
