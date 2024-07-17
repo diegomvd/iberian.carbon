a75d5d01808118ba067697a949fafe70d3d44ab8
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-07-17 18:12:53 +0200

    Fix getitem method for compatibility with kornia AugmentationSequential

--- deep_learning/pnoa_vegetation_nDSM.py
+++ deep_learning/pnoa_vegetation_nDSM.py
@@ -5,6 +5,8 @@ from typing import Any, Callable, Optional, Union
 from rasterio.crs import CRS
 
 from torchgeo.datasets import RasterDataset
+from torchgeo.datasets.utils import BoundingBox
+from typing import Any, cast
 
 
 class PNOAnDSMV(RasterDataset):
@@ -12,9 +14,12 @@ class PNOAnDSMV(RasterDataset):
 
     """
 
-    is_image = False # False
+    is_image = False 
     filename_glob = "NDSM-*-COB*"
     dtype = float32
+
+    filename_regex = r'PNOA_(?P<date>\d{4})'
+    date_format = "%Y"
 
     def __init__(
         self,
@@ -66,3 +71,66 @@ class PNOAnDSMV(RasterDataset):
             "either specify a different `paths` or make sure you "
             "have manually downloaded dataset tiles as suggested in the documentation."
         )
+
+    def __getitem__(self, query: BoundingBox) -> dict[str, Any]:
+        """Retrieve image/mask and metadata indexed by query.
+
+        This implementation overloads the default one by making it compatible with Kornia AgumentationSequential because 
+        TorchGeo AugmentationSequential is deprecated since version 0.4. Specifically the sample dict passed to transform
+        should not contain keys that are not within kornia.augmentation.DataKey enum, like 'crs'. To comply with the
+        requirement transform is only applied to the 'image' or 'mask' key, and the sample  dict is completed afterwards.
+        Note that using Korni AugmentationSequential with a RasterDataset requires setting data_keys to None.
+
+        Args:
+            query: (minx, maxx, miny, maxy, mint, maxt) coordinates to index
+
+        Returns:
+            sample of image/mask and metadata at that index
+
+        Raises:
+            IndexError: if query is not found in the index
+        """
+        hits = self.index.intersection(tuple(query), objects=True)
+        filepaths = cast(list[str], [hit.object for hit in hits])
+
+        if not filepaths:
+            raise IndexError(
+                f'query: {query} not found in index with bounds: {self.bounds}'
+            )
+
+        if self.separate_files:
+            data_list: list[Tensor] = []
+            filename_regex = re.compile(self.filename_regex, re.VERBOSE)
+            for band in self.bands:
+                band_filepaths = []
+                for filepath in filepaths:
+                    filename = os.path.basename(filepath)
+                    directory = os.path.dirname(filepath)
+                    match = re.match(filename_regex, filename)
+                    if match:
+                        if 'band' in match.groupdict():
+                            start = match.start('band')
+                            end = match.end('band')
+                            filename = filename[:start] + band + filename[end:]
+                    filepath = os.path.join(directory, filename)
+                    band_filepaths.append(filepath)
+                data_list.append(self._merge_files(band_filepaths, query))
+            data = torch.cat(data_list)
+        else:
+            data = self._merge_files(filepaths, query, self.band_indexes)
+
+        sample = {}
+
+        data = data.to(self.dtype)
+        if self.is_image:
+            sample['image'] = data
+        else:
+            sample['mask'] = data
+
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+
+        sample['crs'] = self.crs
+        sample['bbox'] = query    
+
+        return sample    
--- deep_learning/sentinel_worldcover_composites.py
+++ deep_learning/sentinel_worldcover_composites.py
@@ -5,20 +5,130 @@ from typing import Any, Callable, Optional, Union
 from rasterio.crs import CRS
 
 from torchgeo.datasets import RasterDataset
+from torchgeo.datasets.utils import BoundingBox
+from typing import Any, cast
 
 class SentinelComposites(RasterDataset):
     is_image = True
-    # filename_regex = r"""
-    #     ^T(?P<tile>\d{{2}}[A-Z]{{3}})
-    #     _(?P<date>\d{{4}}_v)
-    #     _(?P<band>B[018][\dA]) # How to adapt this?
-    #     (?:_(?P<resolution>{}m))?
-    #     \..*$
-    # """
-    # filename_regex = r"""
-    #     _(?P<date>\d{{4}}_v)
-    # """
-    # date_format = "%Y"
+
+    filename_regex = r'ESA_WorldCover_10m_(?P<date>\d{4})'
+    date_format = "%Y"
+
+    def __init__(
+        self,
+        paths: Union[str, list[str]] = "data",
+        crs: Optional[CRS] = CRS.from_epsg(4326),
+        res: Optional[float] = None,
+        transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
+        cache: bool = True,
+    ) -> None:
+        """Initialize a new Dataset instance.
+
+        Args:
+            paths: one or more root directories to search or files to load, here
+                the collection of individual zip files for each tile should be found
+            crs: :term:`coordinate reference system (CRS)` to warp to
+                (defaults to the CRS of the first file found)
+            res: resolution of the dataset in units of CRS
+                (defaults to the resolution of the first file found)
+            transforms: a function/transform that takes an input sample
+                and returns a transformed version
+            cache: if True, cache file handle to speed up repeated sampling
+
+        Raises:
+            FileNotFoundError: if no files are found in ``paths``
+            RuntimeError: if dataset is missing
+
+        .. versionchanged:: 0.5
+           *root* was renamed to *paths*.
+        """
+        self.paths = paths
+
+        self._verify()
+
+        super().__init__(paths, crs, res, transforms=transforms, cache=cache)
+
+
+    def _verify(self) -> None:
+        """Verify the integrity of the dataset.
+
+        Raises:
+            RuntimeError: if dataset is missing
+        """
+        # Check if the extracted files already exists
+        if self.files:
+            return
+
+        raise RuntimeError(
+            f"Dataset not found in `paths={self.paths!r}` "
+            "either specify a different `paths` or make sure you "
+            "have manually downloaded dataset tiles as suggested in the documentation."
+        )
+
+    def __getitem__(self, query: BoundingBox) -> dict[str, Any]:
+        """Retrieve image/mask and metadata indexed by query.
+
+        This implementation overloads the default one by making it compatible with Kornia AgumentationSequential because 
+        TorchGeo AugmentationSequential is deprecated since version 0.4. Specifically the sample dict passed to transform
+        should not contain keys that are not within kornia.augmentation.DataKey enum, like 'crs'. To comply with the
+        requirement transform is only applied to the 'image' or 'mask' key, and the sample  dict is completed afterwards.
+        Note that using Korni AugmentationSequential with a RasterDataset requires setting data_keys to None.
+
+        Args:
+            query: (minx, maxx, miny, maxy, mint, maxt) coordinates to index
+
+        Returns:
+            sample of image/mask and metadata at that index
+
+        Raises:
+            IndexError: if query is not found in the index
+        """
+        hits = self.index.intersection(tuple(query), objects=True)
+        filepaths = cast(list[str], [hit.object for hit in hits])
+
+        if not filepaths:
+            raise IndexError(
+                f'query: {query} not found in index with bounds: {self.bounds}'
+            )
+
+        if self.separate_files:
+            data_list: list[Tensor] = []
+            filename_regex = re.compile(self.filename_regex, re.VERBOSE)
+            for band in self.bands:
+                band_filepaths = []
+                for filepath in filepaths:
+                    filename = os.path.basename(filepath)
+                    directory = os.path.dirname(filepath)
+                    match = re.match(filename_regex, filename)
+                    if match:
+                        if 'band' in match.groupdict():
+                            start = match.start('band')
+                            end = match.end('band')
+                            filename = filename[:start] + band + filename[end:]
+                    filepath = os.path.join(directory, filename)
+                    band_filepaths.append(filepath)
+                data_list.append(self._merge_files(band_filepaths, query))
+            data = torch.cat(data_list)
+        else:
+            data = self._merge_files(filepaths, query, self.band_indexes)
+
+        sample = {}
+
+        data = data.to(self.dtype)
+        if self.is_image:
+            sample['image'] = data
+        else:
+            sample['mask'] = data
+
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+
+        sample['crs'] = self.crs
+        sample['bbox'] = query    
+
+        return sample
+
+    
 
 class Sentinel2RGBNIR(SentinelComposites):
     """Sentinel-2 RGBNIR annual composites for 2020 and 2021.
@@ -26,58 +136,8 @@ class Sentinel2RGBNIR(SentinelComposites):
     """
 
     filename_glob = "ESA_WorldCover_*S2RGBNIR*"
+    all_bands = ['B04','B03','B02','B08']
     
-
-    def __init__(
-        self,
-        paths: Union[str, list[str]] = "data",
-        crs: Optional[CRS] = None,
-        res: Optional[float] = None,
-        transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
-        cache: bool = True,
-    ) -> None:
-        """Initialize a new Dataset instance.
-
-        Args:
-            paths: one or more root directories to search or files to load, here
-                the collection of individual zip files for each tile should be found
-            crs: :term:`coordinate reference system (CRS)` to warp to
-                (defaults to the CRS of the first file found)
-            res: resolution of the dataset in units of CRS
-                (defaults to the resolution of the first file found)
-            transforms: a function/transform that takes an input sample
-                and returns a transformed version
-            cache: if True, cache file handle to speed up repeated sampling
-
-        Raises:
-            FileNotFoundError: if no files are found in ``paths``
-            RuntimeError: if dataset is missing
-
-        .. versionchanged:: 0.5
-           *root* was renamed to *paths*.
-        """
-        self.paths = paths
-
-        self._verify()
-
-        super().__init__(paths, crs, res, transforms=transforms, cache=cache)
-
-
-    def _verify(self) -> None:
-        """Verify the integrity of the dataset.
-
-        Raises:
-            RuntimeError: if dataset is missing
-        """
-        # Check if the extracted files already exists
-        if self.files:
-            return
-
-        raise RuntimeError(
-            f"Dataset not found in `paths={self.paths!r}` "
-            "either specify a different `paths` or make sure you "
-            "have manually downloaded dataset tiles as suggested in the documentation."
-        )
 
 class Sentinel2SWIR(SentinelComposites):
     """Sentinel-2 SWIR annual composites for 2020 and 2021.
@@ -85,115 +145,16 @@ class Sentinel2SWIR(SentinelComposites):
     """
 
     filename_glob = "ESA_WorldCover_*SWIR*"
+    all_bands = ['B12-p50','B11-p50']  
+
     
-    def __init__(
-        self,
-        paths: Union[str, list[str]] = "data",
-        crs: Optional[CRS] = None,
-        res: Optional[float] = None,
-        transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
-        cache: bool = True,
-    ) -> None:
-        """Initialize a new Dataset instance.
-
-        Args:
-            paths: one or more root directories to search or files to load, here
-                the collection of individual zip files for each tile should be found
-            crs: :term:`coordinate reference system (CRS)` to warp to
-                (defaults to the CRS of the first file found)
-            res: resolution of the dataset in units of CRS
-                (defaults to the resolution of the first file found)
-            transforms: a function/transform that takes an input sample
-                and returns a transformed version
-            cache: if True, cache file handle to speed up repeated sampling
-
-        Raises:
-            FileNotFoundError: if no files are found in ``paths``
-            RuntimeError: if dataset is missing
-
-        .. versionchanged:: 0.5
-           *root* was renamed to *paths*.
-        """
-        self.paths = paths
-
-        self._verify()
-
-        super().__init__(paths, crs, res, transforms=transforms, cache=cache)
-
-
-    def _verify(self) -> None:
-        """Verify the integrity of the dataset.
-
-        Raises:
-            RuntimeError: if dataset is missing
-        """
-        # Check if the extracted files already exists
-        if self.files:
-            return
-
-        raise RuntimeError(
-            f"Dataset not found in `paths={self.paths!r}` "
-            "either specify a different `paths` or make sure you "
-            "have manually downloaded dataset tiles as suggested in the documentation."
-        )
 
 class Sentinel2NDVI(SentinelComposites):
     """Sentinel-2 NDVI annual composites for 2020 and 2021.
 
     """
-
     filename_glob = "ESA_WorldCover_*NDVI*"
-    
-    def __init__(
-        self,
-        paths: Union[str, list[str]] = "data",
-        crs: Optional[CRS] = None,
-        res: Optional[float] = None,
-        transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
-        cache: bool = True,
-    ) -> None:
-        """Initialize a new Dataset instance.
-
-        Args:
-            paths: one or more root directories to search or files to load, here
-                the collection of individual zip files for each tile should be found
-            crs: :term:`coordinate reference system (CRS)` to warp to
-                (defaults to the CRS of the first file found)
-            res: resolution of the dataset in units of CRS
-                (defaults to the resolution of the first file found)
-            transforms: a function/transform that takes an input sample
-                and returns a transformed version
-            cache: if True, cache file handle to speed up repeated sampling
-
-        Raises:
-            FileNotFoundError: if no files are found in ``paths``
-            RuntimeError: if dataset is missing
-
-        .. versionchanged:: 0.5
-           *root* was renamed to *paths*.
-        """
-        self.paths = paths
-
-        self._verify()
-
-        super().__init__(paths, crs, res, transforms=transforms, cache=cache)
-
-
-    def _verify(self) -> None:
-        """Verify the integrity of the dataset.
-
-        Raises:
-            RuntimeError: if dataset is missing
-        """
-        # Check if the extracted files already exists
-        if self.files:
-            return
-
-        raise RuntimeError(
-            f"Dataset not found in `paths={self.paths!r}` "
-            "either specify a different `paths` or make sure you "
-            "have manually downloaded dataset tiles as suggested in the documentation."
-        )        
+    all_bands = ['NDVI-p90', 'NDVI-p50', 'NDVI-p10']
 
 class Sentinel1(SentinelComposites):
     """Sentinel-1 VV, VH, VV/VH annual composites for 2020 and 2021.
@@ -201,54 +162,5 @@ class Sentinel1(SentinelComposites):
     """
 
     filename_glob = "ESA_WorldCover_*S1VVVHratio*"
-    
-    def __init__(
-        self,
-        paths: Union[str, list[str]] = "data",
-        crs: Optional[CRS] = None,
-        res: Optional[float] = None,
-        transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]] = None,
-        cache: bool = True,
-    ) -> None:
-        """Initialize a new Dataset instance.
+    all_bands = ['VV','VH','ratio']
 
-        Args:
-            paths: one or more root directories to search or files to load, here
-                the collection of individual zip files for each tile should be found
-            crs: :term:`coordinate reference system (CRS)` to warp to
-                (defaults to the CRS of the first file found)
-            res: resolution of the dataset in units of CRS
-                (defaults to the resolution of the first file found)
-            transforms: a function/transform that takes an input sample
-                and returns a transformed version
-            cache: if True, cache file handle to speed up repeated sampling
-
-        Raises:
-            FileNotFoundError: if no files are found in ``paths``
-            RuntimeError: if dataset is missing
-
-        .. versionchanged:: 0.5
-           *root* was renamed to *paths*.
-        """
-        self.paths = paths
-
-        self._verify()
-
-        super().__init__(paths, crs, res, transforms=transforms, cache=cache)
-
-
-    def _verify(self) -> None:
-        """Verify the integrity of the dataset.
-
-        Raises:
-            RuntimeError: if dataset is missing
-        """
-        # Check if the extracted files already exists
-        if self.files:
-            return
-
-        raise RuntimeError(
-            f"Dataset not found in `paths={self.paths!r}` "
-            "either specify a different `paths` or make sure you "
-            "have manually downloaded dataset tiles as suggested in the documentation."
-        )        
