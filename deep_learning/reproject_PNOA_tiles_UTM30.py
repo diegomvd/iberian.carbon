f6fdcfc126bab1358c55caa39f560a2cfa4489e0
Diego Bengochea <bengo.bengo@protonmail.com>
2024-07-18 16:44:21 +0200

    Creating target directory

--- deep_learning/reproject_PNOA_tiles_UTM30.py
+++ deep_learning/reproject_PNOA_tiles_UTM30.py
@@ -4,10 +4,13 @@ from pathlib import Path
 dir = '/Users/diegobengochea/git/iberian.carbon/data/Vegetation_NDSM_PNOA2/PNOA2_merged/'
 out_dir = '/Users/diegobengochea/git/iberian.carbon/data/Vegetation_NDSM_PNOA2/PNOA2_merged_UTM30/'
 
+if not Path(out_dir).exists():
+    Path(out_dir).mkdir(parents=True)
+
 print('Reprojecting:')
 for i, file in enumerate(Path(dir).glob('*.tif')):
 
-    print('file {} out of {}'.format(i, len(Path(dir).glob('*.tif'))))
+    print('file {}'.format(i))
 
     output_file = '{}{}_epsg_25830.tif'.format(out_dir,file.stem) 
 
