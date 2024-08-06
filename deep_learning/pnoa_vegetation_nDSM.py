16561021e324c76c33f127a7c772baac716175dc
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-08-06 09:28:44 +0200

    Custom intersection class to handle transforms with non valid datakeys
    This won't be needed after next Kornia release

--- deep_learning/pnoa_vegetation_nDSM.py
+++ deep_learning/pnoa_vegetation_nDSM.py
@@ -9,6 +9,40 @@ from torchgeo.datasets.utils import BoundingBox
 from typing import Any, cast
 
 
+class SentinelPNOAIntersect(IntersectionDataset):
+
+    def __getitem__(self, query: BoundingBox) -> dict[str, Any]:
+        """Retrieve image and metadata indexed by query.
+
+        Args:
+            query: (minx, maxx, miny, maxy, mint, maxt) coordinates to index
+
+        Returns:
+            sample of data/labels and metadata at that index
+
+        Raises:
+            IndexError: if query is not within bounds of the index
+        """
+        if not query.intersects(self.bounds):
+            raise IndexError(
+                f"query: {query} not found in index with bounds: {self.bounds}"
+            )
+
+        # All datasets are guaranteed to have a valid query
+        samples = [ds[query] for ds in self.datasets]
+
+        sample = self.collate_fn(samples)
+
+        # Only pass valid Kornia data keys to transforms. This won't be needed after Kornia's next release (>0.7.4).
+        kornia_sample = {'image':sample['image'], 'mask':sample['mask']}
+
+        if self.transforms is not None:
+            kornia_sample = self.transforms(kornia_sample)
+
+        sample = { 'image' : kornia_sample['image'], 'mask' : kornia_sample['mask'], 'crs': sample['crs'], 'bbox' : sample['bbox'] }
+
+        return sample
+
 class PNOAnDSMV(RasterDataset):
     """Spanish vegetation Normalized Surface Digital Model
 
