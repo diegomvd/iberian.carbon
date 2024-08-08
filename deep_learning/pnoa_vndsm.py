5c28d0e8ccf550756b7518a29913e5cbb398ab00
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-08-08 13:06:32 +0200

    Redesigned geo datasets and datamodule

---
+++ deep_learning/pnoa_vndsm.py
@@ -0,0 +1,16 @@
+from torchgeo.datasets import RasterDataset
+
+class PNOAnDSMV(RasterDataset):
+    """Spanish vegetation Normalized Surface Digital Model
+
+    """
+
+    is_image = False 
+    filename_glob = "PNOA_*"
+
+    filename_regex = r'PNOA_(?P<date>\d{4})'
+    date_format = "%Y"
+
+
+
+ 
