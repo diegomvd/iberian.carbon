d575f5e52fbeddd173edbbebe106dfb65f066f66
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-08-19 16:42:39 +0200

    Update datapath to UTM30 projection for all layers

--- deep_learning/new_model_training.py
+++ deep_learning/new_model_training.py
@@ -9,7 +9,7 @@ from lightning.pytorch.loggers import TensorBoardLogger, CSVLogger
 import torch
 
 path = '/Users/diegobengochea/git/iberian.carbon/data/LightningDataModule_Data/'
-path = '/Users/diegobengochea/git/iberian.carbon/data/dl_test'
+path = '/Users/diegobengochea/git/iberian.carbon/data/dl_test_utm30'
 
 dm = SentinelWorldCoverPNOAVnDSMDataModule(data_dir=path)
 
