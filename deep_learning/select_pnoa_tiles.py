56cf3ef41f84578b13fe03f08e6013cfc29469f0
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-09-11 16:23:35 +0200

    Avoid repetition and prefer UTM30 for PNOA tile selection

--- deep_learning/select_pnoa_tiles.py
+++ deep_learning/select_pnoa_tiles.py
@@ -11,6 +11,10 @@ import numpy  as np
 sentinel_path = '/Users/diegobengochea/git/iberian.carbon/data/LightningDataModule_Data/'
 
 pnoa_path = '/Users/diegobengochea/git/iberian.carbon/Entornos_Vuelo_LiDAR/2_Cobertura/'
+
+pnoa_paths = ['/Users/diegobengochea/git/iberian.carbon/Entornos_Vuelo_LiDAR/2_Cobertura/Huso_30/','/Users/diegobengochea/git/iberian.carbon/Entornos_Vuelo_LiDAR/2_Cobertura/Huso_29/','/Users/diegobengochea/git/iberian.carbon/Entornos_Vuelo_LiDAR/2_Cobertura/Huso_31/']
+
+
 pnoa_datadir =  '/Users/diegobengochea/PNOA2/PNOA2_LIDAR_VEGETATION/'
 
 target_pnoa_datadir = '/Users/diegobengochea/git/iberian.carbon/data/training_pnoa_tiles/'
@@ -18,9 +22,11 @@ target_pnoa_datadir = '/Users/diegobengochea/git/iberian.carbon/data/training_pn
 if not Path(target_pnoa_datadir).exists():
     Path(target_pnoa_datadir).mkdir()
 
-fileids = set()
+selected_new_fnames = []
+
 for year in [2020,2021]:
    for sentinel_tile in Path(sentinel_path).rglob('*{}*SWIR.tif'.format(year)):
+        fileids = set()
         with rasterio.open(sentinel_tile)  as src_sentinel:
             
             bounds_sentinel = src_sentinel.bounds
@@ -28,29 +34,32 @@ for year in [2020,2021]:
 
             tile_list = []
             
-            for pnoa_polygons in Path(pnoa_path).rglob('*.shp'):
-                # Each shapefile contains all rectangular polygons of the area sampled by the plane.
+            for pnoa_path in pnoa_paths:
+                for pnoa_polygons in Path(pnoa_path).glob('*.shp'):
+                    # Each shapefile contains all rectangular polygons of the area sampled by the plane, each pnoa path corresponds to a different UTM.
 
-                utm = re.findall('_HU(..)_',str(pnoa_polygons))[0]
-                pnoa_gdf = gpd.read_file(pnoa_polygons)
-                intersecting_tiles = pnoa_gdf.intersects(polygon_sentinel)
+                    utm = re.findall('_HU(..)_',str(pnoa_polygons))[0]
 
-                # Select only the polygons that intersect the sentinel tile
-                pnoa_intersecting = pnoa_gdf[intersecting_tiles]
-                #  Filter by year
-                pnoa_intersecting = pnoa_intersecting[ pnoa_intersecting.FECHA == str(year)]
-                # Add file names of the PNOA files to intersect.
-                selected_files = pnoa_intersecting['PATH'].apply(lambda x: '{}{}'.format(pnoa_datadir,x.split('/')[-1]) ).to_list()
 
-                selected_files_and_year = [ (f,year) for f in selected_files]
+                    pnoa_gdf = gpd.read_file(pnoa_polygons)
+                    intersecting_tiles = pnoa_gdf.intersects(polygon_sentinel)
 
-                #selected_dict = {re.findall('NDSM-VEGETACION-(?:...)-(.*)-COB2.tif',f)[0] : f for f in selected_files}
-                #selected_dict = { fid : selected_dict[fid] for fid in selected_dict if not fid in fileids }
-                # remove redundant files
-                fileids.update(selected_files_and_year)
+                    # Select only the polygons that intersect the sentinel tile
+                    pnoa_intersecting = pnoa_gdf[intersecting_tiles]
+                    #  Filter by year
+                    pnoa_intersecting = pnoa_intersecting[ pnoa_intersecting.FECHA == str(year)]
+                    # Add file names of the PNOA files to intersect.
+                    selected_files = pnoa_intersecting['PATH'].apply(lambda x: '{}{}'.format(pnoa_datadir,x.split('/')[-1]) ).to_list()
 
-                # Create dict with new name that includes year of sampling!!!!!!! Then resave rasters by renaming. 
-selected_new_fnames = [ (x[0], '{}PNOA_{}_{}'.format(target_pnoa_datadir, x[1], re.findall('.*/(NDSM-.*)',x[0])[0])) for x in fileids ]
+                    # Remove redundant files
+                    selected_dict = {re.findall('NDSM-VEGETACION-(?:...)-(.*)-COB2.tif',f)[0] : f for f in selected_files}
+                    selected_dict = { fid : selected_dict[fid] for fid in selected_dict if not fid in fileids }
+                    
+                    fileids.update(selected_dict.keys)
+
+                    for f in selected_dict.values():
+                        selected_new_fnames.append( (f, '{}PNOA_{}_{}'.format(target_pnoa_datadir, year, re.findall('.*/(NDSM-.*)',f)[0]))  )
+
 
 for ref, target in selected_new_fnames:
     with rasterio.open(ref) as src: 
@@ -65,7 +74,7 @@ for ref, target in selected_new_fnames:
             dtype=np.float32,
             count=1,
             nodata=float(src.nodata),
-            crs="EPSG:258{}".format(utm),
+            crs=src.crs,
             transform=src.transform,
             compress='lzw'
         ) as new_dataset:
@@ -73,24 +82,7 @@ for ref, target in selected_new_fnames:
         
 
              
-# if len(merging_dict)>0:
-#     esa_id = re.findall('v.00_(.*)_SWIR.tif',str(sentinel_tile))[0]
-#     merge_name = '/Users/diegobengochea/PNOA2_merged/PNOA_{}_H{}_{}_NDSM.tif'.format(year,utm,esa_id)
-#     image, transform = merge( list(merging_dict.values()) )
-#     with rasterio.open(
-#         merge_name,
-#         mode="w",
-#         driver="GTiff",
-#         height=image.shape[-2],
-#         width=image.shape[-1],
-#         dtype=np.float32,
-#         count=1,
-#         nodata=-32767.0,
-#         crs="EPSG:258{}".format(utm),
-#         transform=transform,
-#         compress='lzw'
-#     ) as new_dataset:
-#         new_dataset.write(image[0,:,:], 1) 
+
                         
 
 
