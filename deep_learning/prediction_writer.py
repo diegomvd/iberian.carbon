aa3f89b6c9f72f4da63fc2a3886c93cd50bebdde
Diego Bengochea <diego.bengocheapaz@pm.me>
2024-09-12 22:13:07 +0200

    Draft of custom writer to create canopy height rasters out of prediciton

---
+++ deep_learning/prediction_writer.py
@@ -0,0 +1,28 @@
+import torch
+from lightning.pytorch.callbacks import BasePredictionWriter
+
+class CanopyHeightRasterWriter(BasePredictionWriter):
+
+    def __init__(self, output_dir, write_interval):
+        super().__init__(write_interval)
+        self.output_dir = output_dir
+
+    def write_on_batch_end(
+        self, trainer, pl_module, prediction, batch_indices, batch, batch_idx, dataloader_idx
+    ):
+        print('BATCH')
+        print(batch)
+        print('BATCH INDEX')
+        print(batch_idx)
+        print('BATCH INDICES')
+        print(batch_indices)
+        print('DATALOADER INDEX')
+        print(dataloader_idx)
+
+        # In function of how comes the information just use the tensor image to build a raster with the corresponding raster bounds.
+
+        # torch.save(prediction, os.path.join(self.output_dir, dataloader_idx, f"{batch_idx}.pt"))
+
+    def write_on_epoch_end(self, trainer, pl_module, predictions, batch_indices):
+        # torch.save(predictions, os.path.join(self.output_dir, "predictions.pt"))
+        return 'BLABLA'
