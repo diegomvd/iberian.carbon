92e37ae60c399fa79a5e84bdc2e53d37bae775c1
Diego Bengochea <bengo.bengo@protonmail.com>
2024-09-13 10:22:57 +0200

    Resolved merge conflict

--- deep_learning/model_training.py
+++ deep_learning/model_training.py
@@ -50,9 +50,9 @@ trainer = Trainer(
 )
 
 resume_from_checkpoint = True
-stage = 'predict' # 'test'
-stage = 'test'
-stage = 'fit'
+stage = 'predict' 
+#stage = 'test'
+#stage = 'fit'
 
 
 if resume_from_checkpoint:
@@ -60,7 +60,7 @@ if resume_from_checkpoint:
         test_metrics = trainer.test(unet_regression, datamodule = dm, ckpt_path ="/Users/diegobengochea/git/iberian.carbon/deep_learning/epoch=2-step=234.ckpt")
         print(test_metrics)
     elif stage == 'predict':
-        prediction = trainer.predict(unet_regression, datamodule = dm, ckpt_path = "/Users/diegobengochea/git/iberian.carbon/deep_learning/epoch=2-step=234.ckpt")
+        prediction = trainer.predict(unet_regression, datamodule = dm, ckpt_path = "/Users/diegobengochea/git/iberian.carbon/deep_learning/model_0_weights/epoch=2-step=234.ckpt")
         print(prediction)
     else:
         trainer.fit(unet_regression, datamodule=dm, ckpt_path="/Users/diegobengochea/git/iberian.carbon/deep_learning/model_0_weights/epoch=2-step=234.ckpt")
