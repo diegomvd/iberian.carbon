526a6566296ffbd0bac1ec104006f0dbe6083e7d
Diego Bengochea <bengo.bengo@protonmail.com>
2024-11-05 20:51:43 +0100

    Switch to CNIG S2 composites

--- deep_learning/sentinel2_composites.py
+++ deep_learning/sentinel2_composites.py
@@ -1,16 +1,47 @@
 """Sentinel-2 green-season composites for 2017 to 2024."""
 
-from torchgeo.datasets import RasterDataset
+from torchgeo.datasets import RasterDataset, IntersectionDataset
+
+
+    
+
+        
 
 class Sentinel2Composite(RasterDataset):
     is_image = True
 
-    filename_glob = "sentinel2_mosaic_*"
-    filename_regex = r'sentinel2_mosaic_(?P<date>\d{4})'
+    filename_regex = r'pnt_sentinel2_(?P<date>\d{4})'
     date_format = "%Y"
 
-    all_bands = ['red','green','blue','nir','swir16','swir22','rededge1','rededge2','rededge3','nir08']
+class Sentinel2RGB(Sentinel2Composite):
+
+    filename_glob = "pnt_sentinel2_*_b432_*"
+
+    all_bands = ['Red','Green','Blue']
 
     separate_files = False
 
-    nan_value = 0
+    #nan_value = 0
+
+class Sentinel2IRC(Sentinel2Composite):
+
+    filename_glob = "pnt_sentinel2_*_b843_*"
+
+    all_bands = ['Nir']
+
+    separate_files = False
+
+    #nan_value = 0    
+
+class Sentinel2(IntersectionDataset):
+    all_bands = ['Red','Green','Blue','Nir']
+
+    def __init__(
+
+        self,
+        dataset_rgb: Sentinel2RGB,
+        dataset_irc: Sentinel2IRC,
+    ) -> None:
+
+        super().__init__(dataset_rgb,dataset_irc)    
+
