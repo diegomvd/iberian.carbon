b6089b74dde03db4c6a53a1e9c031a408cafcf8e
Diego <diego.bengocheapaz@pm.me>
2024-11-07 14:56:12 +0100

    Fix MacOS multiprocessing

--- deep_learning/merge_prediction_patches_parallel.py
+++ deep_learning/merge_prediction_patches_parallel.py
@@ -117,11 +117,11 @@ client = dask.distributed.Client(cluster)
 # This is useful if all prediction would be ready, in the meanwhile select manually the years to merge
 # all_files = Path(path).glob('*.tif')
 # years = {re.search(r'_mint_(*).tif',fname.stem).group() for fname in all_files}
+if __name__ == '__main__':
+    years = [1514761200,1546297200,1577833200]
+    args_list = list(itertools.product(geotiles_spain, years))
 
-years = [1514761200,1546297200,1577833200]
-args_list = list(itertools.product(geotiles_spain, years))
-
-for args in args_list:
-    futures = client.submit(merge_and_save,args[0],args[1])
-                
-client.gather(futures)
+    for args in args_list:
+        futures = client.submit(merge_and_save,args[0],args[1])
+                    
+    client.gather(futures)
